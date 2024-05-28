76fb46f6286a4baac841a046b96cebbeb7a66b4f
Sivuyile Mene <smene98@gmail.com>
2024-05-28 13:17:16 +0200

    style: formatted eslint.config.js file

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import { fixupConfigRules } from '@eslint/compat'
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
-];
+  ...fixupConfigRules(pluginReactConfig)
+]
